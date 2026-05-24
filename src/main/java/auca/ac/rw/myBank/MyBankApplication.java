4d91419bdb5a09ab75412448536ce2b425c30c14
Guhirwa <christianguhirwa74@gmail.com>
2026-05-24 17:53:56 +0200

    build(app): add other project generated files
    - via spring initializr I got the ready made template

---
+++ src/main/java/auca/ac/rw/myBank/MyBankApplication.java
@@ -0,0 +1,13 @@
+package auca.ac.rw.myBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankApplication.class, args);
+	}
+
+}
